eea4b97db52ddab231ee0daf042f2e375589ec88
Frank Chu <39079548+MaJesTySA@users.noreply.github.com>
2019-04-27 19:50:36 +0800

    Update LoginServlet.java

--- CaptchaCode/src/code/LoginServlet.java
+++ CaptchaCode/src/code/LoginServlet.java
@@ -17,12 +17,26 @@ public class LoginServlet extends HttpServlet {
         String code = request.getParameter("code");
         //获取自己生成的验证码
         String gcode=(String)request.getSession().getAttribute("gcode");
+        //获取自己生成的算术验证码
+        String gcalc_code=(String)request.getSession().getAttribute("gcalc_code");
         //获取验证码框架产生的验证码（会话中存储的验证码）
         String kcode = (String)request.getSession().getAttribute("kcode");
-        if(flag.equals("1")){
+        
+        if(flag.equals("0")){
             if(code!=null&gcode!=null) {
                 //如果用户输入的验证码和产生在服务器端的验证码一致，那么就告诉用户输入正确
                 if (code.equalsIgnoreCase(gcode)) {
+                    //登录逻辑、注册逻辑等相关的业务操作
+                    out.print("success");
+                } else {
+                    out.print("fail");
+                }
+            }
+        }
+        if(flag.equals("1")){
+            if(code!=null&gcalc_code!=null) {
+                //如果用户输入的验证码和产生在服务器端的验证码一致，那么就告诉用户输入正确
+                if (code.equalsIgnoreCase(gcalc_code)) {
                     //登录逻辑、注册逻辑等相关的业务操作
                     out.print("success");
                 } else {
