b0e7fb2173da3475277f75a7f7d4d677f986382c
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-05-30 18:08:07 +0800

    Create ImportExcelResultDto.java

---
+++ ImExport_Excel_And_Word/src/dto/ImportExcelResultDto.java
@@ -0,0 +1,34 @@
+package dto;
+
+import entity.Student;
+import java.util.List;
+
+public class ImportExcelResultDto {
+    private String title;
+    private List<Student> studentList;
+    private String msg;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
