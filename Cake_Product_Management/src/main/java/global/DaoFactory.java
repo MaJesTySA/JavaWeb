2c1b413a3da95edc091f27275ad67a8c25a35579
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-06-02 11:26:44 +0800

    Create DaoFactory.java

---
+++ Cake_Product_Management/src/main/java/global/DaoFactory.java
@@ -0,0 +1,28 @@
+package global;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+    private SqlSessionFactory sessionFactory;
+    private DaoFactory(){
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder=new SqlSessionFactoryBuilder();
+        try {
+            sessionFactory=sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("/mybatis.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static DaoFactory getInstance(){
+        if(daoFactory==null){
+            daoFactory=new DaoFactory();
+        }
+        return daoFactory;
+    }
+    public <T> T getDao(Class<T> tClass){
+        return sessionFactory.openSession(true).getMapper(tClass);
+    }
+}
