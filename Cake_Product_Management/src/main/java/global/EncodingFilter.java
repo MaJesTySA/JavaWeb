f38cbac23c22645e6d088a99c836633961789742
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-06-02 11:26:08 +0800

    Create EncodingFilter.java

---
+++ Cake_Product_Management/src/main/java/global/EncodingFilter.java
@@ -0,0 +1,26 @@
+package global;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodingFilter",urlPatterns ="/*",
+        initParams = {@WebInitParam(name = "encoding",value = "UTF-8")})
+public class EncodingFilter implements Filter {
+    private String encoding="UTF-8";
+    public void init(FilterConfig config) throws ServletException {
+        if(config.getInitParameter("encoding")!=null){
+            encoding=config.getInitParameter("encoding");
+        }
+    }
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest hreq=(HttpServletRequest)req;
+        HttpServletResponse hresp=(HttpServletResponse)resp;
+        hreq.setCharacterEncoding(encoding);
+        hresp.setCharacterEncoding(encoding);
+        chain.doFilter(hreq, hresp);
+    }
+}
